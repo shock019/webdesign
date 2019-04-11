8ae7bd5b98c1b748e8a275c8442c19633c03a76f
s <shock9182@gmail.com>
2019-04-11 13:05:54 -0400

    d

--- e17/js/color.js
+++ e17/js/color.js
@@ -1,33 +1,35 @@
 $(document).ready(function() {
     // run function on initial page load
 	colorGuess();
+
 });
 
 function colorGuess() {
-	var colorArray = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
-	var randomNumber = Math.floor(Math.random() * colorArray.length);
-	var stripped = colorArray[randomNumber].toLowerCase();
-	console.log(colorArray[randomNumber]);
-	console.log(stripped);
-	$('body').css('background-color', colorArray[randomNumber]);
+	var imagearray = ["url(image/Chickennoodle.jpg)","url(image/tomatobasil.jpg)"];
+	var randomNumber = Math.floor(Math.random() * imagearray.length);
+	console.log(imagearray[randomNumber]);
+	$("#container").css("background-image", imagearray[randomNumber]);
+	//console.log(stripped);
+	//$('body').css('bg', colorArray[randomNumber]);
 	$('#submit').click(function() {
-		var input = $('input').val();
-		var guess = input.toLowerCase();
-		console.log(guess);
-		if (guess == stripped || guess == colorArray[randomNumber]) {
-			$('h1').text(guess + ' is right!');
-			$('input').val('');
-			$('.button p').text('Play Again?');
-			$('#submit').click(function() {
-				location.reload(true);
-			});
-		} else  {
-			$('h1').text(guess + ' is wrong');
-			$('input').val('');
-			$('.button p').text('Play Again?');
-			$('#submit').click(function() {
-				location.reload(true);
-			});
-		}
+		// var input = $('input').val();
+		// var guess = input.toLowerCase();
+		$("#container").css("background-image", imagearray[randomNumber]);
+		// console.log(guess);
+		// if (guess == stripped || guess == colorArray[randomNumber]) {
+		// 	$('h1').text(guess + ' is right!');
+		// 	$('input').val('');
+		// 	$('.button p').text('Play Again?');
+		// 	$('#submit').click(function() {
+		// 		location.reload(true);
+		// 	});
+		// } else  {
+		// 	$('h1').text(guess + ' is wrong');
+		// 	$('input').val('');
+		// 	$('.button p').text('Play Again?');
+		// 	$('#submit').click(function() {
+		// 		location.reload(true);
+		// 	});
+		//}
 	})
 }
